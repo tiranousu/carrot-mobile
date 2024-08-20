114a25dd875b3fb0d1e367bf169613f8e02cb9b8
tiranousu <tiranous@naver.com>
2024-08-20 10:17:54 +0900

    당근마켓 클론 코딩
    -기본 ui구성
    -회원가입 로직 구현
    -로그인 페이지로 이동

--- main.py
+++ main.py
@@ -10,36 +10,31 @@ cur = con.cursor()
 
 app = FastAPI()
 
-@app.post('/items')
-async def create_item(image: UploadFile, 
-                      title: Annotated[str, Form()],
-                      price: Annotated[int, Form()],
-                      description: Annotated[str, Form()],
-                      place: Annotated[str, Form()],
-                      insertAt: Annotated[int, Form()]):
-    
-    image_bytes = await image.read()
-    cur.execute(f"""
-                INSERT INTO items (title, image, price, description, place, insertAt)
-                VALUES ('{title}', '{image_bytes.hex()}', {price}, '{description}', '{place}', {insertAt})
-                """)
-    con.commit()
-    return '200'
+@app.post("/signup")
+def signup(id: Annotated[str, Form()],
+           password: Annotated[str, Form()],
+           name: Annotated[str, Form()],
+           email: Annotated[str, Form()]):
+    cur.execute("""
+                INSERT INTO users(id, name, email, password)
+                VALUES (?, ?, ?, ?)
+                """, (id, name, email, password))
+    con.commit()  # 'comit'을 'commit'으로 수정
+    return "200"
 
-
-@app.get('/items')  # 슬래시가 누락되어 있었음
+@app.get('/items')
 async def get_items():
     con.row_factory = sqlite3.Row
     cur = con.cursor()
     rows = cur.execute("SELECT * FROM items").fetchall()
-    return JSONResponse(content=jsonable_encoder([dict(row) for row in rows]))  # 이 부분 수정됨
+    return JSONResponse(content=jsonable_encoder([dict(row) for row in rows]))
 
 @app.get('/images/{item_id}')
 async def get_image(item_id):
     cur = con.cursor()
-    image_bytes = cur.execute(f"""
-                              SELECT image from items WHERE id = {item_id}
-                              """).fetchone()[0]
-    return Response(content = bytes.fromhex(image_bytes))
+    image_bytes = cur.execute("""
+                              SELECT image FROM items WHERE id = ?
+                              """, (item_id,)).fetchone()[0]
+    return Response(content=bytes.fromhex(image_bytes))
 
 app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
